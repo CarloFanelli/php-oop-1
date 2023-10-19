92523914d4e92a59550e36fea4e2d452c7a52157
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-19 12:45:50 +0200

    add constructor

--- index.php
+++ index.php
@@ -15,4 +15,22 @@ class Movie
     public $genre;
 
     public $rating;
+
+    function __construct(
+        $title,
+        $description,
+        $img_url,
+        $cast,
+        $language,
+        $genre,
+        $rating,
+    ) {
+        $this->title = $title;
+        $this->description = $description;
+        $this->img_url = $img_url;
+        $this->cast = $cast;
+        $this->language = $language;
+        $this->genre = $genre;
+        $this->rating = $rating;
+    }
 }
