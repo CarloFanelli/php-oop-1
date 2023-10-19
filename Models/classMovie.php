c0545e1169339962c7576ea90b51ba495a2b6e12
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-19 16:04:46 +0200

    add classMovie with movie class

---
+++ Models/classMovie.php
@@ -0,0 +1,51 @@
+<?php
+class Movie
+{
+    public $title;
+
+    public $description;
+
+    public $img_url;
+
+    public $cast;
+
+    public $genre;
+    function __construct(
+        $title,
+        $description,
+        $img_url,
+        $cast,
+        $genre
+    ) {
+        $this->title = $title;
+        $this->description = $description;
+        $this->img_url = $img_url;
+        $this->cast = $cast;
+        $this->genre = $genre;
+    }
+
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function getDescription()
+    {
+        return $this->description;
+    }
+
+    function getImg()
+    {
+        return $this->img_url;
+    }
+
+    function getGenre()
+    {
+        return $this->genre;
+    }
+
+    function getCast()
+    {
+        return $this->cast;
+    }
+}
--- index.php
+++ index.php
@@ -1,58 +1,6 @@
 <?php
+include __DIR__ . '/Models/classMovie.php';
 include __DIR__ . '/db.php';
-class Movie
-{
-    public $title;
-
-    public $description;
-
-    public $img_url;
-
-    public $cast;
-
-    public $genre;
-    function __construct(
-        $title,
-        $description,
-        $img_url,
-        $cast,
-        $genre
-    ) {
-        $this->title = $title;
-        $this->description = $description;
-        $this->img_url = $img_url;
-        $this->cast = $cast;
-        $this->genre = $genre;
-    }
-
-    function getTitle()
-    {
-        return $this->title;
-    }
-
-    function getDescription()
-    {
-        return $this->description;
-    }
-
-    function getImg()
-    {
-        return $this->img_url;
-    }
-
-    function getGenre()
-    {
-        return $this->genre;
-    }
-
-    function getCast()
-    {
-        return $this->cast;
-    }
-}
-
-
-//var_dump($movies);
 
 ?>
 
