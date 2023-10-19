7b6ae1d4be3ad15611b7a869cc6b2877b38bf221
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-19 12:36:59 +0200

    declaire instance variables

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+class Movie
+{
+    public $title;
+
+    public $description;
+
+    public $img_url;
+
+    public $cast;
+
+    public $language;
+
+    public $genre;
+
+    public $rating;
+}
