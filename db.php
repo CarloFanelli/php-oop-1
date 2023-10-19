ae5e8d0770fd38c08fd6cfdc55846e70d1adeb1c
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-19 16:07:03 +0200

    edit dp.php file with semplified array_push

--- db.php
+++ db.php
@@ -1,20 +1,24 @@
 <?php
 $movies = [];
 
-$matrix = new Movie(
-    'Matrix',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est?',
-    'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
-    ['claudio bisio', 'lino banfi'],
-    ['action', 'drama']
-);
+array_push(
+    $movies,
+    $matrix = new Movie(
+        'Matrix',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est?',
+        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        ['claudio bisio', 'lino banfi'],
+        ['action', 'drama']
+    ),
 
-$madagascar = new Movie(
-    'Madagascar',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est?',
-    'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
-    ['Alex il leone', 'la zebra', 'la giraffa'],
-    ['commedia', 'animazione']
-);
+    $madagascar = new Movie(
+        'Madagascar',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est?',
+        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        ['Alex il leone', 'la zebra', 'la giraffa'],
+        ['commedia', 'animazione']
+    )
 
-array_push($movies, $matrix, $madagascar);
+    /* add here next movie */
+
+);
